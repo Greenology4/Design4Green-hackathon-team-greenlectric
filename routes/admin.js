be72a5707bb85db3811624ea908bd190841ffc68
Salim Huneidi <a92920@aup.edu>
2019-11-21 21:06:49 +0100

    added function to upload a document and consumption for a house for admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 
 //Get all houses info 
-router.get('/', isLoggedIn, (req, res) => {
+router.get('/', (req, res) => {
    
     fs.readFile(file_path, (err, data) => {
         if (err) throw err;
@@ -17,50 +17,54 @@ router.get('/', isLoggedIn, (req, res) => {
     });
 });
 
-router.post('/',isLoggedIn ,upload.single('myFile'), (req, res) => {
+router.post('/',upload.single('myFile'), (req, res) => {
+    console.log('Here');
     
-    var house = req.body.house ;     
-    var path = req.file.path;
-    var file_path = 'file_paths.json';
-
-    fs.readFile(file_path, (err, data) => {
-        let info = JSON.parse(data);
-            info[house] = {};
-            info[house] = path;
-            fs.writeFile(file_path, JSON.stringify(info),(err, data)=> {
-                res.sendStatus(200);
-                if(err) throw err;
-            })
-            if(err) throw err
+    if(req.body.consumption){
+        console.log("1");
         
-    });
-   //var file1 = req.file.myFile
-   
-   
-
+        let id = req.body.house;
+        let user_date = req.body.date
+        let consumption = req.body.consumption;
+        var now = new Date();
+        var date = user_date || dateFormat(now, "dd/mm/yyyy");
+        var file_path = 'data1.json';
+        fs.readFile(file_path, (err, data) => {
+            let info = JSON.parse(data);
+            if(info.consumption[id]){
+                info.consumption[id][date] = {};
+                info.consumption[id][date] = consumption;
+                fs.writeFile(file_path, JSON.stringify(info),(err, data)=> {
+                    res.sendStatus(200);
+                    if(err) throw err;
+                })
+            } else {
+                throw err
+            }
+            
+        });
+    } else if(req.file) {
+        console.log("2");
+        var house = req.body.house ;     
+        var path = req.file.path;
+        var file_path = 'file_paths.json';
+        fs.readFile(file_path, (err, data) => {
+            let info = JSON.parse(data);
+                info[house] = {};
+                info[house] = path;
+                fs.writeFile(file_path, JSON.stringify(info),(err, data)=> {
+                    res.sendStatus(200);
+                    if(err) throw err;
+                })
+                if(err) throw err
+        });
+    }
+    else{
+        console.log("3");
+        
+    }
 });
 
-router.post('/:id', isLoggedIn, (req, res) => {
-    let id = req.params.id;
-    let consumption = req.body.consumption;
-    var now = new Date();
-    var date = dateFormat(now, "dd/mm/yyyy");
-    
-    fs.readFile(file_path, (err, data) => {
-        let info = JSON.parse(data);
-        if(info.consumption[id]){
-            info.consumption[id][date] = {};
-            info.consumption[id][date] = consumption;
-            fs.writeFile(file_path, JSON.stringify(info),(err, data)=> {
-                res.sendStatus(200);
-                if(err) throw err;
-            })
-        } else {
-            throw err
-        }
-        
-    });
-})
 
 function isLoggedIn(req, res, next) {
     console.log(req.session.info);
