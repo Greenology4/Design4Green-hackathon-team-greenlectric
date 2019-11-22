f854596277c277400a9e430d226eba22f0b54070
Subramanian <subru1997@gmail.com>
2019-11-22 06:31:47 +0100

    Add reset password

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
-
+const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs')
 
 const file_path = 'data1.json';
@@ -14,7 +14,6 @@ router.get('/login', (req, res) => {
 
 router.post('/login', (req, res, next) => {
     
-    
     var username = req.body.username;
     var password = req.body.password;
     fs.readFile(file_path, (err, data) => {
@@ -24,11 +23,11 @@ router.post('/login', (req, res, next) => {
             if(bcrypt.compareSync(password, info.users[username]['password'])) {
                 if(info.users.D4G2019 === info.users[username] ){
                     req.session.info = {};
-                    req.session.info.username = Object.keys(info.users)[0];
+                    req.session.info.username = username;
                     return res.redirect('admin')
                 } else{  
                     req.session.info = {};
-                    req.session.info.username = Object.keys(info.users)[0];
+                    req.session.info.username = username;
                     req.session.info.foyer = info.users[username]['Foyer'];
                     req.session.info.house = info.Houses[req.session.info.foyer]
                     req.session.info.owner = info.owner[req.session.info.foyer]
@@ -117,5 +116,61 @@ router.post('/signup', (req, res) => {
             return res.redirect('/signup');
         }
 });
+
+router.get('/forgot', (req, res) => {
+    return res.render('forgot-password');
+});
+
+router.post('/forgot', (req, res) => {
+    var email = req.body.email;
+    var username = req.body.username;
+
+    new_password = Math.random().toString(36).slice(2);
+
+    bcrypt.hash(new_password, 10, (err, hash) => {
+        fs.readFile(file_path, (err, data) => {
+            if (err) throw err;
+
+            var info = JSON.parse(data);
+            info.users[username]['password'] = hash;
+
+            var transporter = nodemailer.createTransport({
+                host: "smtp-mail.outlook.com",
+                secureConnection: false, 
+                port: 587,
+                tls: {
+                  ciphers:'SSLv3'
+                },
+                auth: {
+                  user: "greenology4green@outlook.com",
+                //   pass: process.env.MAIL_SYSTEM_PASSWORD
+                pass: 'Greenology5'
+                }
+            });
+
+            const mailOptions = {
+                from: 'greenology4green@outlook.com', // sender address
+                to: email, // list of receivers
+                subject: 'Your password has been reset', // Subject line
+                html: `<p>Bonjour ${username}, <br> Your new password is ${new_password}</p>`// plain text body
+            };
+
+            transporter.sendMail(mailOptions, function (err, res1) {
+                if(err) {
+                    console.log(err)
+                } else {
+                    fs.writeFile(file_path, JSON.stringify(info), function(err1) {
+                        if(err1) {
+                            return console.log(err1);
+                        }
+                        return res.redirect('/login');
+                    });
+                }
+             });  
+            
+        });
+    });
+});
+
 
 module.exports = router;
